071b90c16699194ab4fc5805256000949c22ae9e
Edward Mason <mynameisprivate3@gmail.com>
2025-03-08 13:48:22 +0000

    Update "title" in navbar (Part: thoughts)

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,19 @@
 const Nav = ({ title }) => {
-    return (
-        <nav className="bg-white border-black border-b-2 font-mono">
-            <ul className="flex space-x-4 justify-between">
-                <li className="text-black p-4 font-bold">
-                    {title}
-                </li>
-                <div className="flex space-x-4">
-                    <li className="text-black hover:bg-gray-400 p-4">
-                        <a href="/">Home</a>
-                    </li>
-                    <li className="text-black hover:bg-gray-400 p-4">
-                        <a href="/thoughts">Thoughts</a>
-                    </li>
-                </div>
-            </ul>
-        </nav>
-    );
+	return (
+		<nav className="bg-white border-black border-b-2 font-mono">
+			<ul className="flex space-x-4 justify-between">
+				<li className="text-black text-xl p-4 font-bold">{title}</li>
+				<div className="flex space-x-4">
+					<li className="text-black hover:bg-gray-400 p-4">
+						<a href="/">Home</a>
+					</li>
+					<li className="text-black hover:bg-gray-400 p-4">
+						<a href="/thoughts">Thoughts</a>
+					</li>
+				</div>
+			</ul>
+		</nav>
+	);
 };
 
 export default Nav;
