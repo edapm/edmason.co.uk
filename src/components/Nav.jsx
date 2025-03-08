16950f2bdc540f2ff659be475ba29dea416a01e8
Edward Mason <mynameisprivate3@gmail.com>
2025-03-08 15:00:48 +0000

    Add RSS feed link to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ const Nav = ({ title }) => {
 	return (
 		<nav className="bg-white border-black border-b-2 font-mono">
 			<ul className="flex space-x-4 justify-between">
-				<li className="text-black text-xl p-4 font-bold">{title}</li>
+				<li className="text-black p-4 font-bold">{title}</li>
 				<div className="flex space-x-4">
 					<li className="text-black hover:bg-gray-400 p-4">
 						<a href="/">Home</a>
@@ -10,8 +10,14 @@ const Nav = ({ title }) => {
 					<li className="text-black hover:bg-gray-400 p-4">
 						<a href="/thoughts">Thoughts</a>
 					</li>
+
 					<li className="text-black hover:bg-gray-400 p-4">
 						<a href="/arch">Archives</a>
+					</li>
+					<li className="text-black hover:bg-orange-500 p-4">
+						<a href="/thoughts/rss.xml">
+							<span class="iconify-inline" data-icon="ri:rss-fill"></span>
+						</a>
 					</li>
 				</div>
 			</ul>
