b72014db6681a87a4187fcaa5cce3a704f5cec32
Edward Mason <mynameisprivate3@gmail.com>
2025-03-15 15:03:10 +0000

    Add sitemap, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://edmason.co.uk",
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  integrations: [react()]
+	site: "https://edmason.co.uk",
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [react(), sitemap()],
 });
