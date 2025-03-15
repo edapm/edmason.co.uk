7930160161fcc46f404fe42d4b48935e6d74bc56
Edward Mason <mynameisprivate3@gmail.com>
2025-03-15 12:26:13 +0000

    Fix some rss parsing issues

--- src/pages/thoughts/rss.xml.js
+++ src/pages/thoughts/rss.xml.js
@@ -2,7 +2,7 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import sanitizeHtml from "sanitize-html";
 import MarkdownIt from "markdown-it";
-const parser = new MarkdownIt();
+const parser = new MarkdownIt({ html: true });
 
 export async function GET(context) {
 	const thoughts = await getCollection("thoughts");
@@ -13,10 +13,16 @@ export async function GET(context) {
 		site: context.site,
 		items: thoughts.map((post) => ({
 			link: `/thoughts/${post.id}/`,
+			title: post.data.title,
+			pubDate: post.data.date,
 			// Note: this will not process components or JSX expressions in MDX files.
 			content: sanitizeHtml(parser.render(post.body), {
-				allowedTags: [],
-				allowedAttributes: {},
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img", "iframe"]),
+				allowedAttributes: {
+					a: ["href", "name", "target"],
+					img: ["src"],
+					iframe: ["src", "width", "height", "frameborder", "allowfullscreen"],
+				},
 			}),
 			...post.data,
 		})),
