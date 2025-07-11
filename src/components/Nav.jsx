4f05d02ac1fb04cdf8a1a57da41e9182661eead0
Edward Mason <mynameisprivate3@gmail.com>
2025-07-11 09:33:14 +0100

    Add responsive nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,9 @@
 const Nav = ({ title }) => {
 	return (
-		<nav className="bg-white border-black border-b-2 font-mono">
+		<nav className="block bg-white border-black border-b-2 font-mono">
 			<ul className="flex space-x-4 justify-between">
 				<li className="text-black p-4 font-bold">{title}</li>
-				<div className="flex space-x-4">
+				<div className="space-x-4 hidden md:flex">
 					<li className="text-black hover:bg-gray-400 p-4">
 						<a href="/">Home</a>
 					</li>
